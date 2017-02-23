509fda51652529b724b25bebdcea8cbd4ecd748d
Hans Lemuet <hans@etaminstudio.com>
2017-02-23 17:35:30 +0100

    Added menu creation + assignation

--- install.sh
+++ install.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
-echo "Qui est motivé pour écrire ce script ?"
+echo "Création des menus"
+wp menu create "Menu principal"
+wp menu location assign "Menu principal" "principal"
+wp menu create "Menu secondaire"
+wp menu location assign "Menu secondaire" "secondary"
+wp menu create "Pied de page"
+wp menu location assign "Pied de page" "footer-bar"
+wp menu create "Plan du site"
+wp menu location assign "Plan du site" "footer-columns"
+wp menu location list
 
 exit 0
